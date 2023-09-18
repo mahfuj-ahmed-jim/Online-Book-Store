b93cbc803e02d1bb6d31870f916d857b3ffb0469
mahfuj-ahmed-jim <mahfujahmedjim32@gmail.com>
2023-09-18 06:13:19 +0600

    Minimize code for discount count

--- controllers/book_controller.js
+++ controllers/book_controller.js
@@ -1,7 +1,7 @@
 const AuthorModel = require("../models/author_model");
 const BookModel = require("../models/book_model");
 const DiscountModel = require("../models/discount_model");
-const { sendResponse } = require("../utils/common");
+const { sendResponse, discountQuery, countBookDiscount } = require("../utils/common");
 const STATUS_CODE = require("../constants/status_codes");
 const STATUS_REPONSE = require("../constants/status_response");
 const RESPONSE_MESSAGE = require("../constants/response_message");
@@ -29,34 +29,9 @@ class BookController {
             });
             authorIds = Array.from(uniqueAuthors);
 
-            const discountQuery = {
-                $or: [
-                    { books: { $in: bookIds } },
-                    { authors: { $in: authorIds } },
-                ],
-            };
-
-            const discounts = await DiscountModel.find(discountQuery);
-
-            const booksWithDiscounts = books.map((book) => {
-                const discount = discounts.find((discount) => {
-                    return (
-                        discount.books.includes(book._id) ||
-                        discount.authors.includes(book.author._id)
-                    );
-                });
-
-                if (discount) {
-                    if (discount.discountPercentage) {
-                        const discountPrice = (book.price / 100) * discount.discountPercentage;
-                        return { ...book.toObject(), discountPrice: book.price - discountPrice };
-                    } else if (discount.discountAmount) {
-                        return { ...book.toObject(), discountPrice: book.price - discount.discountAmount };
-                    }
-                } else {
-                    return book;
-                }
-            });
+            const query = discountQuery(bookIds, authorIds);
+            const discounts = await DiscountModel.find(query);
+            const booksWithDiscounts = countBookDiscount(books, discounts);
 
             return sendResponse(
                 res,
--- utils/common.js
+++ utils/common.js
@@ -13,4 +13,43 @@ const sendResponse = (res, statusCode, message, result = null) => {
   res.status(statusCode).send(response);
 };
 
-module.exports = { sendResponse };
+const discountQuery = (bookIds, authorIds) => {
+  const query = {
+    $and: [
+      { validFrom: { $lte: new Date() } },
+      { validTo: { $gte: new Date() } },
+    ],
+    $or: [
+      { books: { $in: bookIds } },
+      { authors: { $in: authorIds } },
+    ],
+  };
+
+  return query;
+}
+
+const countBookDiscount = (books, discounts) => {
+  const booksWithDiscounts = books.map((book) => {
+    const discount = discounts.find((discount) => {
+      return (
+        discount.books.includes(book._id) ||
+        discount.authors.includes(book.author._id)
+      );
+    });
+
+    if (discount) {
+      if (discount.discountPercentage) {
+        const discountPrice = (book.price / 100) * discount.discountPercentage;
+        return { ...book.toObject(), discountPrice: book.price - discountPrice };
+      } else if (discount.discountAmount) {
+        return { ...book.toObject(), discountPrice: book.price - discount.discountAmount };
+      }
+    } else {
+      return book;
+    }
+  });
+
+  return booksWithDiscounts;
+}
+
+module.exports = { sendResponse, discountQuery, countBookDiscount };
